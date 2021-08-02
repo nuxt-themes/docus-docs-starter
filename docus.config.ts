4625b98a13ee26c72c4bf060e2d8093f741e6402
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-08-03 00:21:22 +0200

    :tada: (init) import old starter; update it to new Docus

---
+++ docus.config.ts
@@ -0,0 +1,19 @@
+export default {
+  title: 'Docus',
+  description: 'Write in markdown, use Vue components, add style with Windi CSS and enjoy the power of Nuxt.',
+  url: 'https://docus.dev',
+  twitter: 'nuxtlabs',
+  template: 'docs',
+  credits: true,
+  github: {
+    url: 'https://github.com/',
+    repo: 'docusgen/starter',
+    branch: 'main',
+    releases: true
+  },
+  theme: {
+    header: {
+      title: true
+    }
+  }
+}
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+import { withDocus } from '@docus/app'
+
+export default withDocus({
+  // TODO: Remove that temporary fix
+  // @ts-ignore
+  vite: {
+    server: {
+      fs: {
+        strict: false
+      }
+    }
+  },
+
+  /**
+   * Has to specify rootDir as we use nuxt-extend
+   */
+  rootDir: __dirname,
+  /**
+   * Modules
+   */
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@docus/github',
+    '@docus/twitter',
+    '@docus/social-image'
+  ],
+})
