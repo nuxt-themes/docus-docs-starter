53afcc360314651e5c22093ecec3598cc0dd95ba
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-11-19 15:32:07 +0100

    :sparkles: (update) update starter to latest docs-theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { withDocus } from '@docus/app/kit'
 
 export default withDocus({
   /**
-   * Has to specify rootDir as we use nuxt-extend
+   * Has to specify rootDir as we merge theme and app.
    */
-  rootDir: __dirname,
+  rootDir: __dirname
 })
