16f64df5877442ceeb26a15e1dd1fa86a85d6c1b
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-29 05:52:44 +0200

    chore(cleanup): cleanup repo

--- .history/nuxt.config_20211209112839.ts
+++
@@ -1,8 +0,0 @@
-import { withDocus } from 'docus'
-
-export default withDocus({
-  /**
-   * Has to specify rootDir as we merge theme and app.
-   */
-  rootDir: __dirname
-})
--- .history/nuxt.config_20220429052237.ts
+++
@@ -1,5 +0,0 @@
-import { defineNuxtConfig } from 'nuxt'
-
-export default defineNuxtConfig({
-  extends: ['./node_modules/docus/theme']
-})
